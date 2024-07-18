925e798e945ac1546836bfc63bd459ab6a51b9b8
asalam <tokku.co@gmail.com>
2024-07-18 23:20:31 +0700

    first commit

---
+++ app/_components/Header.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useUser, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+function Header() {
+  const { user, isSignedIn } = useUser();
+  return (
+    <div className="p-5 flex justify-between items-center border shadow-sm ">
+      <div className ='flex flex-row items-center'>
+        {/* Logo */}
+        <Image src={'/chart-donut.svg'} alt="logo" width={40} height={25} />
+        <span className ='text-red-800 font-bold text-xl '>BinaSmart</span>
+      </div>
+      {isSignedIn ? (<UserButton />) : (
+        <div className ='flex gap-3 items-center'>
+            <Link href='/dashboard'>
+              <Button variant="outline" className ='rounded-full'>Dashboard</Button>
+            </Link>
+            <Link href='/dashboard'>
+              <Button className ='rounded-full'>Get Started</Button>
+            </Link>
+        </div>
+     
+
+
+
+      )}
+
+    </div>
+  );
+}
+
+export default Header;
---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "BinaSmart",
+  description: "AI powered construction budget advisor",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
